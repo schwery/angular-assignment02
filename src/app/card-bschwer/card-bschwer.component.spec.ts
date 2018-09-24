a876c3acd5f8d1ba05739acc0d1818a874a9be30
Brandon Schwery <bschwer@ilstu.edu>
2018-09-23 23:32:01 -0500

    updated card-bschwer

--- src/app/card-bschwer/card-bschwer.component.spec.ts
+++ src/app/card-bschwer/card-bschwer.component.spec.ts
@@ -1,27 +1,25 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
-describe('AppComponent', () => {
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CardBschwerComponent } from './card-bschwer.component';
+
+describe('CardBschwerComponent', () => {
+  let component: CardBschwerComponent;
+  let fixture: ComponentFixture<CardBschwerComponent>;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
+      declarations: [ CardBschwerComponent ]
+    })
+    .compileComponents();
   }));
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'angular-assignment02'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-assignment02');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CardBschwerComponent);
+    component = fixture.componentInstance;
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-assignment02!');
-  }));
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 });
--- src/app/card-bschwer/card-bschwer.component.ts
+++ src/app/card-bschwer/card-bschwer.component.ts
@@ -1,21 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Student } from '../student';
 
 @Component({
-  selector: 'card-keschae',
-  templateUrl: './card-keschae.component.html',
-  styleUrls: ['./card-keschae.component.css']
+  selector: 'card-bschwer',
+  templateUrl: './card-bschwer.component.html',
+  styleUrls: ['./card-bschwer.component.css']
 })
-export class CardKeschaeComponent implements OnInit {
+export class CardBschwerComponent implements OnInit {
 
   me: Student;
 
+  @Output() cardClick:EventEmitter<Student> = new EventEmitter<Student>();
+
   constructor() {
+
+    this.me = {
+      ulid: 'bschwer',
+      name: 'Brandon Schwery',
+      desc_short: 'Senior majoring in Information Systems',
+      desc_long: 'Senior web developer student and part-time web developer for the International Housewares Association.',
+      major: 'Information Systems: Web Application Development',
+      platform: 'Angular',
+      hometown: 'Antioch, IL',
+      hobbies: 'Golf, Videogames',
+      inspiration: 'Brett Mackie',
+      website: 'StackOverflow',
+      school: 'Illinois State University'
+    };
 
   }
 
 
   ngOnInit() {
   }
+
+  sendClickedStudent(){
+    this.cardClick.emit(this.me);
+  }
+
 
 }
